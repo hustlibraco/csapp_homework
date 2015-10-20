44cd5b34dbcda85911723a1decc55e02af37a121
hustlibraco <hustlibraco@gmail.com>
2015-10-20 21:33:47 +0800

    逻辑错误

--- chap2/ex2-91.c
+++ chap2/ex2-91.c
@@ -24,8 +24,12 @@ unsigned f2u(float a) {
 }
 
 float_bits float_absval(float_bits f) {
-    unsigned sign = f >> 31; 
-    return sign == 1 ? (f & 0x7fffffff) : f;
+    unsigned exp = f >> 23 & 0xff;
+    unsigned frac = f & 0x7fffff;
+    if (exp == 0xff && frac == 0) {
+        return f;
+    }
+    return (exp << 23) | frac;
 }
 
 int main(void)
